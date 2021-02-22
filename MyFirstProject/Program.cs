351a325b93e47796d1892827973bfb53699c4c5a
Igor Kamerzan <ink3091245@gmail.com>
2021-02-22 14:54:44 +0300

    Добавьте файлы проекта.

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
