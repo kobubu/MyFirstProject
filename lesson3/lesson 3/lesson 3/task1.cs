9536ef9b535a5d44085525b77846d6ee859e36d1
kobubu <79473433+kobubu@users.noreply.github.com>
2021-03-03 01:48:32 +0300

    Add files via upload

---
+++ lesson3/lesson 3/lesson 3/task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lesson_3
+{
+    class task1
+    {
+
+        //Написать программу, выводящую элементы двухмерного массива по диагонали.
+        //
+        static void Main(string[] args)
+        {
+
+            // Объявляем двумерный массив
+            int[,] myArr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; 
+
+            
+            // перебираем элементы массива и выводим те, индексы которых совпадают: т.е. по диагонали
+            for (int i = 0; i < 3; i++)
+            {
+                
+                for (int j = 0; j < 3; j++)
+                {
+                   if (i == j)
+                    { 
+                    Console.Write(myArr[i, j]);
+                    }
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ lesson3/lesson 3/task2/task2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace task2
+{
+    class task2
+    {
+
+
+        // Написать программу — телефонный справочник — создать двумерный массив 5*2,
+        // хранящий список телефонных контактов: первый элемент хранит имя контакта,
+        // второй — номер телефона/e-mail.
+        static void Main(string[] args)
+        {
+
+
+            string[,] phoneNumbers = new string[5, 2];
+
+
+            Console.WriteLine("Заполни фамилии");
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                {
+                    Console.WriteLine($"Укажи фамилию № " + (i+1));
+                    phoneNumbers[i, 0] = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Заполни номера телефонов");
+            for (int i = 0; i < 5; i++)
+            {
+                {
+                    Console.WriteLine($"Укажи номер телефона фамилии № " + (i+1));
+                    phoneNumbers[i, 1] = Console.ReadLine();
+                }
+            }
+
+
+            // перебор элементов массива
+            for (int i = 0; i < 5; i++)
+            {
+                                {
+                    Console.WriteLine($"{phoneNumbers[i, 0]}  {phoneNumbers[i, 1]}");
+                }
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
