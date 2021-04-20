87e2ca8a215618b20f273f1d746c0bbf76ece08c
kobubu <79473433+kobubu@users.noreply.github.com>
2021-04-20 22:37:47 +0300

    Add files via upload

---
+++ algo/algoles2/algoles2/Node.cs
@@ -0,0 +1,51 @@
+﻿namespace GeekBrainsTests
+{
+    public class Node : ILinkedList
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+
+        public void AddNode(int value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetCount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveNode(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveNode(Node node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+}
---
+++ algo/algoles2/algoles2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace algoles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+            BinarySearch(a , 3);
+            
+        }
+
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    int c = mid + 1;
+                    Console.WriteLine($"Порядковый нумер числа в списке равняется: {c}");
+                    return mid;
+                    
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            Console.WriteLine("Нет такой буквы на табло");
+            return -1;
+        }
+    }
+}
