342c9dd82c18e08b8f04c104340d5752fc40274d
kobubu <79473433+kobubu@users.noreply.github.com>
2021-10-14 02:05:39 +0300

    Add files via upload

---
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(1000, 2000, ac_type.debit_account);
+            { 
+            }
+
+            account1.Output_data();
+        }
+    }
+}
